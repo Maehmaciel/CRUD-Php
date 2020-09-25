8955acc8f348e36f156c4f8ff48b9aca94b7e694
Maria Maciel <mariaeugeniamaciel@gmail.com>
2020-09-25 10:57:09 -0300

    crud php

--- banco.sql
+++ banco.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE `pessoas` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `nome` varchar(100) NOT NULL,
--- connection.php
+++ connection.php
@@ -2,8 +2,8 @@
 error_reporting(E_ALL);
 $servername = "localhost";
 $database = "example";
-$username = "userphp";
-$password = "12345";
+$username = "";
+$password = "";
 $link = mysqli_connect($servername, $username, $password, $database);
 if ($link->connect_error){
 die("Conexão com o banco falhou: " . $conn->connect_error);
